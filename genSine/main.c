0debc8836a12d7a87960546d6b7287c8e0bb4415
meng tony <guthmanny@meng-tonymatoMacBook-Pro.local>
2013-08-20 23:24:29 +0800

    Initial Commit

---
+++ genSine/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  genSine
+//
+//  Created by meng tony on 13-8-20.
+//  Copyright (c) 2013年 meng tony. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
